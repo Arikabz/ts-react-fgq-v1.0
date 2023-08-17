beb9de0d75ee8f308453e23daa6609dbabea3265
Arikabz <arturo.guza1999@gmail.com>
2023-08-16 22:33:10 -0600

    no errors, babyyyy

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,5 +1,7 @@
+import { useState } from "react"
 
 const Select = ({thisWeek, onChange, num}) => {
+    const [current, setCurrent] = useState(thisWeek)
     let opts = []
 
     for(let i=1; i<=num; i++){
@@ -7,14 +9,12 @@ const Select = ({thisWeek, onChange, num}) => {
     }
     function handleChange(e){
         onChange(e.target.value)
+        setCurrent(e.target.value)
     }
 
     return (
-        <select className="select w-full max-w-xs" onChange={e=>handleChange(e)} >
+        <select className="select w-full max-w-xs" value={current} onChange={e=>handleChange(e)} >
             {opts.map(x=>{
-                if(x===Number(thisWeek)){
-                return <option value={x} key={x} selected>Week {x}</option>
-                }
                 return <option value={x} key={x}>Week {x}</option>
             })}
         </select>
